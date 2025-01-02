dc927c3ad75f7eec6d4a8aa2261311775cc9a9f0
RiteshJat <jatritesh05@gmail.com>
2025-01-02 20:05:48 +0530

    add initial configuration and files for Tailwind CSS and other dependencies

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,33 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+    
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("home page requested");
+
+        // Sending data to the view
+        model.addAttribute("name", "Substring");
+        model.addAttribute("youTube", "Hare Krishna");
+        model.addAttribute("id", 123);
+        model.addAttribute("gitHub_Repo", "https://github.com/substring/substring");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        System.out.println("about page requested");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services() {
+        System.out.println("services page requested");
+        return "services";
+    }
+}
